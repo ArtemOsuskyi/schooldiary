09db67049730bece3ad1fa3aa89533d28a7bdb15
a.osuskyi <a.osuskiy@gmail.com>
2022-07-22 14:20:17 +0300

    Templates for Student, Teacher, Schedule; DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { TypeOrmModule }   from "@nestjs/typeorm";
 import { DbConfig }        from "./db/db.config";
 import * as Entities       from './db/entities/index'
 import { AppConfigModule } from "./configs/app.config.module";
+import { StudentModule }   from './student/student.module';
+import { TeacherModule }   from './teacher/teacher.module';
+import { ScheduleModule }  from './schedule/schedule.module';
 import { ConfigModule }    from "@nestjs/config";
 import { AppService }      from "./app.service";
 
@@ -38,8 +41,11 @@ import { AppService }      from "./app.service";
           synchronize,
           entities: Object.values(Entities)
         }
-      }
+      },
     }),
+    StudentModule,
+    TeacherModule,
+    ScheduleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
